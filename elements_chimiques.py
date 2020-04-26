47ca20aa0c73d505b0ffaec3f8940653f53dedc5
YoungMind44 <61629970+YoungMind44@users.noreply.github.com>
2020-04-26 15:54:16 +0000

    Create elements_chimiques.py

---
+++ elements_chimiques.py
@@ -0,0 +1,7 @@
+elements_chimiques = {
+   """ Ce dictionnaire contiendra les éléments chimiques comme "key" et ses caractéristiques
+       type masse molaire, le nbr Z... comme valeur.
+       Ce dictionnaire sera importé vers le script "tableau_periodique.py" pour être utilisé.
+       """
+       
+ }
